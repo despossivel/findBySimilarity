ba44680e38a4b712c9a49366468886d260eebe1f
Matheus Brito <mattbmoller@gmail.com>
2021-10-19 17:00:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ const findBySimilarity = (list = [], strings = '', keys = [], terms = normalize(
       .some(t => keys
       .some(k => normalize( el[ k ] ).toLowerCase().includes( t ) ) ) )
   }
+
+
+
+
+
+
+const alphabet = [...Array(26)].map((q, w) => String.fromCharCode(w + 97))
+
+const ARRAYDATA = [{ name: "Abff" }, { name: "Ba" }, { name: "C" },]
+
+const RESULTADO = alphabet.map(letter => findBySimilarity(ARRAYDATA, letter, ['name']))
